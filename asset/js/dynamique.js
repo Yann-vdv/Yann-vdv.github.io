ce02b37e32faa6bec89615e932f31ee25572afbe
Yann-vdv <61906860+Yann-vdv@users.noreply.github.com>
2021-10-04 15:54:37 +0200

    version 2 du Site
    Cette nouvelle version semble satisfaisante pour remplacer la précédente, d'autres amélioration son cependant à prévoir

--- asset/js/dynamique.js
+++ asset/js/dynamique.js
@@ -2,7 +2,9 @@ function afficher_div(nom) {
     var div = document.getElementById(nom);
     if (div.style.display == "none" || div.style.display == "") {
         div.style.display = "block";
+        document.getElementById(nom + "_button").innerHTML = "voir moins";
     } else {
         div.style.display = "none";
+        document.getElementById(nom + "_button").innerHTML = "voir plus";
     }
 }
